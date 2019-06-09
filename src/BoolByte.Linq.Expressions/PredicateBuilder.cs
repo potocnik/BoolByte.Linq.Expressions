6ecfe74d213091ca8ffbd29b4e24ebaccb19d198
Ales Potocnik Hahonina <ales.potocnik>
2019-06-09 14:17:11 +0100

    Setup

---
+++ src/BoolByte.Linq.Expressions/PredicateBuilder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace BoolByte.Linq.Expressions
+{
+    public class PredicateBuilder
+    {
+        public Expression<Func<T, bool>> Build<T>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/BoolByte.Linq.Expressions.UnitTests/PredicateBuilder/Build.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace BoolByte.Linq.Expressions.UnitTests.PredicateBuilder
+{
+    public class Build
+    {
+        [Test]
+        public void BuildsPredicate()
+        {
+            var testSubject = new Expressions.PredicateBuilder();
+            var actual = testSubject.Build<object>();
+            actual.Should().NotBeNull();
+        }
+    }
+}
